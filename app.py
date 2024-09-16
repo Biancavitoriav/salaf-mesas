e484aa35a921abcc29006962181a5eed9e0e0f5e
Biancavitoriav <124395956+Biancavitoriav@users.noreply.github.com>
2024-09-16 10:47:29 -0300

    Adicionando css ao site

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ mesas = []
  
 # Função para distribuir os alunos nas mesas
 def distribuir_alunos():
-    print("oieee rodei")
     global mesas
     alunos = total_alunos.copy()
     random.shuffle(alunos)
@@ -42,19 +41,191 @@ def schedule_task():
 @app.route('/')
 def show_mesas():
     html_template = '''
-    <html>
-        <head>
-            <title>Distribuição de Alunos</title>
-        </head>
-        <body>
-            <h1>Distribuição de Alunos nas Mesas</h1>
-            {% for i, mesa in enumerate(mesas, 1) %}
-                <h2>Mesa {{ i }}:</h2>
-                <p>{{ ', '.join(mesa) }}</p>
-            {% endfor %}
-            <img src="{{ url_for('static', filename='mapademesa.png') }}" alt="" height="300px">
-        </body>
-    </html>
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Rodizio de mesa</h1>
+    <div class="grid">
+        <div style="color: #9eb7cf;" class="professor">
+            <h3>Mesa do professor</h3>
+        </div>
+
+        <div class="corredor ">
+            <h3 class="vertical-text" style="color: #9eb7cf;">Corredor</h3>
+        </div>
+        <div class="lousa">
+            <h3>Lousa</h3>
+        </div>
+        <div class="mesa2">
+            <div class="circulo">
+                <h2>Mesa 2</h2>
+            </div>
+            <div class="alunos">
+                    <ul>
+                        {% for aluno in mesas[1] %}
+                            <li>{{ aluno }}</li>
+                        {% endfor %}
+                    </ul>
+                </div>
+        </div>
+        <div class="mesa3">
+            <div class="circulo">
+                <h2>Mesa 3</h2>
+            </div>
+            <div class="alunos">
+                    <ul>
+                        {% for aluno in mesas[2] %}
+                            <li>{{ aluno }}</li>
+                        {% endfor %}
+                    </ul>
+                </div>
+        </div>
+        <div class="mesa4">
+            <div class="circulo">
+                <h2>Mesa 4</h2>
+            </div>
+            <div class="alunos">
+                    <ul>
+                        {% for aluno in mesas[3] %}
+                            <li>{{ aluno }}</li>
+                        {% endfor %}
+                    </ul>
+                </div>
+        </div>
+        <div class="mesa5">
+            <div class="circulo">
+                <h2>Mesa 5</h2>
+            </div>
+            <div class="alunos">
+                    <ul>
+                        {% for aluno in mesas[4] %}
+                            <li>{{ aluno }}</li>
+                        {% endfor %}
+                    </ul>
+                </div>
+        </div>
+        <div class="mesa1">
+            <div class="circulo">
+                <h2>Mesa 1</h2>
+            </div>
+            <div class="alunos">
+                    <ul>
+                        {% for aluno in mesas[0] %}
+                            <li>{{ aluno }}</li>
+                        {% endfor %}
+                    </ul>
+                </div>
+        </div>
+        
+    </div>
+</body>
+<style> 
+    .professor{
+        grid-area: p;
+        background-color: #181848  ;
+        margin: 30px;
+        margin-right: 0px;
+        
+    
+    }
+    .professor h3{
+        padding: 20px;
+        text-align: center;
+
+
+    }
+    .lousa{
+        grid-area: l;
+        background-color: #181848  ;
+        
+    
+    }
+    .lousa h3{
+        padding: 10px;
+        text-align: center;
+        color: #9eb7cf;
+
+
+    }
+    .vertical-text{
+        transform: rotate(270deg); 
+        padding: 20px;/* Rotaciona o texto em 90 graus */
+        padding-left: 10px;
+        font-size: 16px;
+        
+    }
+    body{
+        background-color: #9eb7cf;
+    }
+    h1{
+        
+        color: #181848 !important;
+        text-align: center;
+    }
+    .grid {
+    display: grid;
+    grid-template-columns: 130px  auto auto auto; /* Define 5 colunas */
+    grid-template-rows: repeat(5, auto); /* Define 5 linhas */
+    grid-template-areas:
+        "c l l  l"
+        "c . .  p"
+        "c m1 . m2 "
+        "c . m3 . "
+        "c m4 . m5";
+}
+
+    .corredor{
+        background-color: #181848;
+        grid-area: c;
+        width: 100px;
+        margin-right: 30px;        
+    } 
+
+    .mesa1{
+        display: flex;
+        grid-area: m1;
+    } 
+    .mesa2{
+        display: flex;
+        grid-area: m2;
+    } 
+    .mesa3{
+        display: flex;
+        grid-area: m3;
+    } 
+    .mesa4{
+        display: flex;
+        grid-area: m4;
+    } 
+    .mesa5{
+        display: flex;
+        grid-area: m5;
+    }
+    .circulo{
+        width: 200px;
+        height: 200px;
+        border: 3px solid #181848;
+        border-radius: 200px;
+        
+        
+    }
+    .aluno{
+        align-items: end;
+        
+    }
+    h2{
+        text-align: center;
+        justify-content: center ;
+        padding-top: 60px;
+    }
+</style>
+</html>
+
     '''
     return render_template_string(html_template, mesas=mesas, enumerate=enumerate)
  
@@ -62,3 +233,4 @@ if __name__ == '__main__':
     distribuir_alunos()  # Inicializa as mesas na primeira execução
     schedule_task()  # Agenda a tarefa para rodar todos os dias às 6 da manhã
     app.run(host='0.0.0.0', port=5000)
+    print("oieee rodei")
